bfb028d3e361ec849b778548abd8d331cfddf036
JunsongDu <junsong.du@eonerc.rwth-aachen.de>
2024-04-16 11:14:14 +0200

    feat: implement test append attribute

--- validation_tests/test_data_model.py
+++ validation_tests/test_data_model.py
@@ -1,15 +1,101 @@
+import time
 import unittest
 import json
 import os.path
+from functools import wraps
+from typing import Callable
+from paho.mqtt.client import Client
 from filip.clients.ngsi_v2 import ContextBrokerClient, IoTAClient
 from filip.models import FiwareHeader
-from filip.models.ngsi_v2.context import ContextEntity
-from filip.models.ngsi_v2.iot import Device
-from filip.utils.cleanup import clear_all
+from filip.models.ngsi_v2.context import ContextEntity, NamedContextAttribute
+from filip.models.ngsi_v2.iot import Device, ServiceGroup, DeviceAttribute
+from filip.utils.cleanup import clear_all, clean_test
 
 from settings import settings
 
 path_input = "inputs/test_data_model"
+
+standard_entity = {
+    "id": "Entity:001",
+    "type": "Entity",
+    "attribute1": {
+        "type": "Number",
+        "value": 1
+    },
+    "attribute2": {
+        "type": "Number",
+        "value": 2
+    }
+}
+
+standard_device = {
+    "device_id": "Device:001",
+    "entity_name": standard_entity["id"],
+    "entity_type": standard_entity["type"],
+    "transport": "MQTT",
+    "explicitAttrs": True,
+    "attributes": [
+        {
+            "name": "attribute1",
+            "type": "Number",
+            "object_id": "a1"
+        },
+        {
+            "name": "attribute2",
+            "type": "Number",
+            "object_id": "a2"
+        }
+    ]
+}
+
+standard_service_group = {
+    "resource": "/iot/json",
+    "apikey": "fiware-api-test",
+    "explicitAttrs": True,
+    "autoprovision": False
+}
+
+
+def standard_test(*,
+                  fiware_service: str,
+                  fiware_servicepath: str,
+                  cb_url: str = None,
+                  iota_url: str = None,
+                  ql_url: str = None,
+                  ) -> Callable:
+    # clean up
+    fiware_header = FiwareHeader(service=fiware_service,
+                                 service_path=fiware_servicepath)
+    clear_all(fiware_header=fiware_header,
+              cb_url=cb_url,
+              iota_url=iota_url,
+              ql_url=ql_url)
+
+    # initial entity
+    entity = ContextEntity(**standard_entity)
+    with ContextBrokerClient(url=cb_url, fiware_header=fiware_header) as cbc:
+        cbc.post_entity(entity=entity)
+
+    # initial service group
+    service_group = ServiceGroup(**standard_service_group)
+    with IoTAClient(url=iota_url, fiware_header=fiware_header) as iotac:
+        iotac.post_group(service_group=service_group)
+
+    # initial device
+    device = Device(**standard_device)
+    with IoTAClient(url=iota_url, fiware_header=fiware_header) as iotac:
+        iotac.post_device(device=device)
+
+    # wrapper
+    def decorator(func):
+        #  Wrapper function for the decorated function
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
 
 
 class TestDataModel(unittest.TestCase):
@@ -20,16 +106,20 @@ class TestDataModel(unittest.TestCase):
             service_path=settings.FIWARE_SERVICEPATH,
         )
 
-        clear_all(fiware_header=self.fiware_header,
-                  cb_url=settings.CB_URL,
-                  iota_url=settings.IOTA_JSON_URL
-                  )
+        # clear_all(fiware_header=self.fiware_header,
+        #           cb_url=settings.CB_URL,
+        #           iota_url=settings.IOTA_JSON_URL
+        #           )
 
         # Initial client
         self.cb_client = ContextBrokerClient(url=settings.CB_URL,
                                              fiware_header=self.fiware_header)
         self.iotc = IoTAClient(url=settings.IOTA_JSON_URL,
                                fiware_header=self.fiware_header)
+
+        self.mqttc = Client()
+        self.mqttc.connect(host=settings.MQTT_BROKER_URL.host,
+                           port=settings.MQTT_BROKER_URL.port)
 
         # device list
         self.devices_list = [
@@ -111,16 +201,93 @@ class TestDataModel(unittest.TestCase):
                 entity_attr = entity.get_attribute(attr.name)
                 self.assertEqual(entity_attr.type, attr.type)
 
+    @standard_test(
+        fiware_service=settings.FIWARE_SERVICE,
+        fiware_servicepath=settings.FIWARE_SERVICEPATH,
+        cb_url=settings.CB_URL,
+        iota_url=settings.IOTA_JSON_URL)
     def test_append_attribute(self):
-        pass
+        """
+        New attributes are appended
+        """
+        # append attributes in CB
+        new_attribute_name = "attribute3"
+        self.cb_client.update_or_append_entity_attributes(
+            entity_id=standard_entity["id"],
+            entity_type=standard_entity["type"],
+            attrs=[
+                NamedContextAttribute(**{
+                    "name": "attribute3",
+                    "type": "Number",
+                    "value": 3})],
+            append_strict=True)
+        # test sending, and fetching, should fail
+        topic = f"/json/{standard_service_group['apikey']}/{standard_device['device_id']}/attrs"
+        new_value = 33
+        self.mqttc.publish(topic=topic, payload=json.dumps({"attribute3": new_value}))
+        time.sleep(0.5)
+        self.assertNotEqual(new_value,
+                            self.cb_client.get_attribute_value(
+                                entity_id=standard_entity["id"],
+                                entity_type=standard_entity["type"],
+                                attr_name=new_attribute_name
+                            ))
+        # append attributes in IoTAgent
+        device = self.iotc.get_device(device_id=standard_device["device_id"])
+        device.add_attribute(attribute=DeviceAttribute(
+            name=new_attribute_name,
+            type="Number",
+            object_id=new_attribute_name
+        ))
+        # standard_device["attributes"].append({
+        #     "name": new_attribute_name,
+        #     "type": "Number",
+        #     "object_id": new_attribute_name
+        # })
+        self.iotc.update_device(device=device)
+        time.sleep(0.5)
+
+        # test sending again, should work
+        self.mqttc.publish(topic=topic, payload=json.dumps({"attribute3": new_value}))
+        time.sleep(0.5)
+        self.assertEqual(new_value,
+                         self.cb_client.get_attribute_value(
+                             entity_id=standard_entity["id"],
+                             entity_type=standard_entity["type"],
+                             attr_name=new_attribute_name
+                         ))
 
     def test_delete_attribute(self):
+        """
+        Attributes are deleted
+        :return:
+        """
+        # delete attributes in IoTAgent
+        # test sending, should fail
+        # test fetching, attribute should be found
+        # delete attributes in CB
+        # test fetching, attribute should not be found
         pass
 
     def test_rename_attribute(self):
+        """
+        Attributes are renamed
+        :return:
+        """
+        # rename attributes in IoTAgent
+        # test sending, and fetching with new name, should fail
+        # append new attribute in CB
+        # test sending and fetching again, should work
         pass
 
     def test_anonymous_update(self):
+        """
+        Anonymous attributes getting updated
+        :return:
+        """
+        # test sending anonymous attributes, and fetching, should fail
+        # append new attribute in CB
+        # test sending and fetching again, should work
         pass
 
     def tearDown(self) -> None:
@@ -128,7 +295,6 @@ class TestDataModel(unittest.TestCase):
             service=settings.FIWARE_SERVICE,
             service_path=settings.FIWARE_SERVICEPATH,
         )
-
         clear_all(fiware_header=self.fiware_header,
                   cb_url=settings.CB_URL,
                   iota_url=settings.IOTA_JSON_URL
